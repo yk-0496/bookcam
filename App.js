3cde29d6ce2cb4affc9ec07bb67d13f0eae50fe6
yk-0496 <yeonseo1243@gmail.com>
2020-10-10 18:47:12 +0900

    trial-youtube

--- App.js
+++ App.js
@@ -1,21 +1,71 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, AppRegistry } from 'react-native';
+import { Camera } from 'expo-camera';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class cameraTutorial extends React.Component {
+  compoenetDidMout() {
+    (async () => {
+      const { status } = await Camera.requestPermissionsAsync();
+      this.setHasPermission(status === 'granted');
+    })();
+  }
+  render() {
+    return (
+      <View style={{
+        flex: 1,
+        backgroundColor: 'transparent',
+        flexDirection: 'row'
+      }}>
+        <Camera
+        //type={this.state.type}
+          ref={ref => {
+          this.camera = ref;
+          }}
+          style={{ width: 300, height: 400}}
+          //aspect={Camera.Constants.Type.front}
+        >
+          <Text style ={StyleSheet.capture} onPress={this.takePicture.bind(this)}>
+            [CAPTURE_IMAGE]
+          </Text>
+        </Camera>
+      </View>
+    );
+  }
+  
+  takePicture() {
+    const options = {}
+    this.camera.capture({metadata: options}).then((data) => {
+      console.log(data)
+    }).catch((error) => {
+      console.log(error)
+    })
+  }
 }
+
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#F5FCFF',
     alignItems: 'center',
-    justifyContent: 'center',
+    //justifyContent: 'center',
   },
+  view: {
+    flex:1,
+    justifyContent: 'flex-end',
+    //alignItems: 'center'
+  },
+  capture: {
+    flex: 0,
+    backgroundColor: 'steelblue',
+    borderRadius: 10,
+    color: 'red',
+    padding: 15,
+    margin: 45
+  }
+
 });
+
+AppRegistry.registerComponent('cameraTutrial', () => cameraTutorial);
