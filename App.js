94ed653147ce2aaa50f8256bc623e68de7b54378
yk-0496 <yeonseo1243@gmail.com>
2020-10-10 19:20:00 +0900

    camera-basic

--- App.js
+++ App.js
@@ -1,71 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View, TouchableOpacity, AppRegistry } from 'react-native';
-import { Camera } from 'expo-camera';
+import CameraPage from './camera.page.js';
 
-export default class cameraTutorial extends React.Component {
-  compoenetDidMout() {
-    (async () => {
-      const { status } = await Camera.requestPermissionsAsync();
-      this.setHasPermission(status === 'granted');
-    })();
-  }
+export default class Application extends React.Component {
   render() {
-    return (
-      <View style={{
-        flex: 1,
-        backgroundColor: 'transparent',
-        flexDirection: 'row'
-      }}>
-        <Camera
-        //type={this.state.type}
-          ref={ref => {
-          this.camera = ref;
-          }}
-          style={{ width: 300, height: 400}}
-          //aspect={Camera.Constants.Type.front}
-        >
-          <Text style ={StyleSheet.capture} onPress={this.takePicture.bind(this)}>
-            [CAPTURE_IMAGE]
-          </Text>
-        </Camera>
-      </View>
+    return(
+      <CameraPage />
     );
-  }
-  
-  takePicture() {
-    const options = {}
-    this.camera.capture({metadata: options}).then((data) => {
-      console.log(data)
-    }).catch((error) => {
-      console.log(error)
-    })
-  }
-}
+  };
+};
 
-
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#F5FCFF',
-    alignItems: 'center',
-    //justifyContent: 'center',
-  },
-  view: {
-    flex:1,
-    justifyContent: 'flex-end',
-    //alignItems: 'center'
-  },
-  capture: {
-    flex: 0,
-    backgroundColor: 'steelblue',
-    borderRadius: 10,
-    color: 'red',
-    padding: 15,
-    margin: 45
-  }
-
-});
-
-AppRegistry.registerComponent('cameraTutrial', () => cameraTutorial);
+//AppRegistry.registerComponent('cameraTutrial', () => cameraTutorial);
